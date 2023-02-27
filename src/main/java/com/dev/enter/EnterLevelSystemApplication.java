d120e16c9ca1cf38c958c2c1b9a966d93c332738
Yeh-Wang <wyx1627082750@163.com>
2023-02-27 15:41:35 +0800

    Yeh Wang

---
+++ src/main/java/com/dev/enter/EnterLevelSystemApplication.java
@@ -0,0 +1,13 @@
+package com.dev.enter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterLevelSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterLevelSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/enter/EnterLevelSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.enter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterLevelSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
