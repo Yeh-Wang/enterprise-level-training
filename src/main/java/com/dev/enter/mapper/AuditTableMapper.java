65839ac13177769f45d046da57451350d10e66bd
Yeh-Wang <wyx1627082750@163.com>
2023-02-28 11:04:10 +0800

    Yeh Wang

---
+++ src/main/java/com/dev/enter/mapper/AuditTableMapper.java
@@ -0,0 +1,17 @@
+package com.dev.enter.mapper;
+
+import com.dev.enter.entity.AuditTableEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @since 2023-02-28 10:59:36
+ */
+@Mapper
+public interface AuditTableMapper extends BaseMapper<AuditTableEntity> {
+
+}
---
+++ src/main/java/com/dev/enter/service/impl/QuestionSourceServiceImpl.java
@@ -0,0 +1,19 @@
+package com.dev.enter.service.impl;
+
+import com.dev.enter.entity.QuestionSourceEntity;
+import com.dev.enter.mapper.QuestionSourceMapper;
+import com.dev.enter.service.QuestionSourceService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @since 2023-02-28 10:59:36
+ */
+@Service
+public class QuestionSourceServiceImpl extends ServiceImpl<QuestionSourceMapper, QuestionSourceEntity> implements QuestionSourceService {
+
+}
