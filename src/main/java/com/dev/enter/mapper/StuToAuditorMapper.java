74cee804373f3cfe2fa6db2cca939d2dd8fc464d
Yeh-Wang <wyx1627082750@163.com>
2023-03-02 10:57:05 +0800

    Yeh Wang

---
+++ src/main/java/com/dev/enter/mapper/StuToAuditorMapper.java
@@ -0,0 +1,14 @@
+package com.dev.enter.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.dev.enter.entity.StuToAuditorEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author : Yaxin-Wang
+ * @date : 2023/3/2
+ */
+@Mapper
+public interface StuToAuditorMapper extends BaseMapper<StuToAuditorEntity> {
+
+}
